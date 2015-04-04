8e040679b31137552cac4cdef6884be468c3b5e6
Vijay Kiran <vijaykiran225@gmail.com>
2015-04-05 01:11:17 +0530

    Adding node support and server config

--- NodeApps/nodeTest1/Call.js
+++ NodeApps/nodeTest1/Call.js
@@ -1,11 +1,21 @@
 var Client = require('node-rest-client').Client;
  
 client = new Client();
- 
+ var args = {
+  data: { test: "hello" },
+  headers:{"Accept": "application/json"} 
+};
+var DisplayContent;
 // direct way 
-client.get("http://localhost:8080/jerseytest/rs/vijay/show/rice/tkm12", function(data, response){
+client.get("http://localhost:8080/jerseytest/rs/agro/placeSearch/rice/tkm12",args, function(data, response){
             // parsed response body as js object 
             console.log(data);
+			DisplayContent=data;
             // raw response 
-           // console.log(response);
+           console.log("response ::::"+response);
+           console.log(DisplayContent['give']);
+           //console.log("content of json is "+JSON.stringify(DisplayContent));
         });
+
+
+
--- NodeApps/nodeTest1/routes/index.js
+++ NodeApps/nodeTest1/routes/index.js
@@ -8,7 +8,66 @@ router.get('/', function(req, res, next) {
 
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' })
+    res.render('helloworld', { title: 'Hello, World! vijay' })
+});
+
+/*Get to place search page*/
+router.get('/place', function(req, res) {
+	var Client = require('node-rest-client').Client;
+ 
+client = new Client();
+ var args = {
+  data: { test: "hello" },
+  headers:{"Accept": "application/json"} 
+};
+var DisplayContent;
+// direct way 
+client.get("http://localhost:8080/jerseytest/rs/agro/placeSearch/rice/tkm12",args, function(data, response){
+            // parsed response body as js object 
+            console.log(data);
+			DisplayContent=data;
+            // raw response 
+           console.log("response ::::"+response);
+           console.log(DisplayContent['give']);
+    		res.render('placeSearch', {
+    			title:"placeSearch",
+		     response: DisplayContent
+		 		})
+           //console.log("content of json is "+JSON.stringify(DisplayContent));
+        });
+
+
+
+
+});
+
+/*Get to crop search page*/
+router.get('/crop', function(req, res) {
+	var Client = require('node-rest-client').Client;
+ 
+client = new Client();
+ var args = {
+  data: { test: "hello" },
+  headers:{"Accept": "application/json"} 
+};
+var DisplayContent;
+// direct way 
+client.get("http://localhost:8080/jerseytest/rs/agro/cropSearch/kanyakumari/rainfed/normal",args, function(data, response){
+            // parsed response body as js object 
+            console.log(data);
+			DisplayContent=data;
+            // raw response 
+           console.log("response ::::"+response);
+           console.log(DisplayContent['places']);
+    res.render('cropSearch', {
+    	title :"cropSearch", 
+    	response: DisplayContent 
+
+    })
+           //console.log("content of json is "+JSON.stringify(DisplayContent));
+        });
+
+
 });
 
 module.exports = router;
